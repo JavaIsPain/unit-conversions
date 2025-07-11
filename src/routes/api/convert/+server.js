86ad10d20329b3cf9112808abbcd5d6aa589da3b
shellsort <cl.mertig@gmail.com>
2025-07-11 23:57:05 +0200

    Created Branch for Jules

--- src/routes/api/convert/+server.js
+++ src/routes/api/convert/+server.js
@@ -3,7 +3,7 @@ import { SULU_API_KEY } from '$env/static/private';
 
 export async function POST({ request }) {
     const { category, fromUnit, toUnit, fromValue } = await request.json();
-
+ 
     const url = `https://unit-conversions.p.sulu.sh/v1/unit-conversions/${category}/${fromUnit}/to/${toUnit}`;
     const options = {
         method: 'POST',
